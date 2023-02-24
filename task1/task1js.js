98cf76fdfe6084378e8996521ea04b2738a07f8e
saurabhIR <saurabh.kumawat@innoraft.com>
2023-02-24 11:21:14 +0530

    FT2023-72-task1  added php DocBlock to js

--- task1/task1js.js
+++ task1/task1js.js
@@ -1,4 +1,8 @@
-// function for displaying full name in disabled input with js.
+/**
+ * Updates a full name field with the combined values of a first and last name field.
+ * 
+ * @returns {void}
+ */
 function updateFullName() {
   var firstName = document.getElementById("firstName").value;
   var lastName = document.getElementById("lastName").value;
